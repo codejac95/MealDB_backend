191fce1cf9c4e54fe0c407de31f90fbb7e24a3bd
Jacob Jansson <41388531+jaken1234@users.noreply.github.com>
2024-02-01 15:42:32 +0100

    lagt till patch

--- src/main/java/MealDB/MealDB/RecipeController.java
+++ src/main/java/MealDB/MealDB/RecipeController.java
@@ -1,19 +1,24 @@
 package MealDB.MealDB;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/recipes")
-@CrossOrigin(origins = "http://127.0.0.1:5501")
+@CrossOrigin(origins = "*")
 public class RecipeController {
     private final RecipeRepository recipeRepository;
 
@@ -23,18 +28,32 @@ public class RecipeController {
     }
     @GetMapping
     public List<Recipe> getAllRecipes() {
-        System.out.println("Alla grejer");
         return (List<Recipe>) recipeRepository.findAll();
     }
 
     @PostMapping("/add")
     public Recipe addRecipe(@RequestBody Recipe recipe) {
-        System.out.println("lagt till en");
         return recipeRepository.save(recipe);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteRecipe(@PathVariable Integer id) {
         recipeRepository.deleteById(id);
     }
+ 
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<Recipe> updateRecipe(@PathVariable Integer id, @RequestBody Recipe updatedRecipe) {
+        Optional<Recipe> optionalRecipe = recipeRepository.findById(id);
+        if (optionalRecipe.isPresent()) {
+            Recipe existingRecipe = optionalRecipe.get();
+            if (updatedRecipe.getComment() != null) {
+                existingRecipe.setComment(updatedRecipe.getComment());
+            }
+            Recipe updated = recipeRepository.save(existingRecipe);
+            return new ResponseEntity<>(updated, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
